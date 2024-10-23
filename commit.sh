3785e0db82ab782d20a044408b230550938913f9
taufiqart <taufiqqurrohman333@gmail.com>
2024-10-23 10:00:58 +0700

    :rocket: refact

--- commit.sh
+++ commit.sh
@@ -1,4 +1,7 @@
+source ${cat .env | grep GIT | sed 's/GIT/export GIT/g'}
 git config --global --add safe.directory ${PWD}
+git config --global user.email ${GIT_EMAIL}
+git config --global user.name ${GIT_USERNAME}
 git add .
 git commit -m "add new case from server"
 git push origin prod
